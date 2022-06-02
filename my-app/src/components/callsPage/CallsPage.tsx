8f04273f90c66980ca9c999b7abf53f6693025f6
Ankook <52046510+Ankook@users.noreply.github.com>
2022-06-02 22:25:14 +0300

    renaming css on scss, callspage is working

---
+++ my-app/src/components/callsPage/CallsPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'; // we need this to make JSX compile
+
+
+
+const CallsPage: React.FC = () => {
+  return (
+    <div>
+      CallsPage
+    </div>
+  )
+} 
+
+export {CallsPage}
