75fbf4f358ab20e397ee5d167dfd37fbf6fef9b8
Ankook <52046510+Ankook@users.noreply.github.com>
2022-06-03 22:10:11 +0300

    creating callEvaluationStatusTextComponent

--- my-app/src/components/Ui-Kit/CallEvaluationTextStatus/CallEvaluationTextStatus.tsx
+++ my-app/src/components/Ui-Kit/CallEvaluationTextStatus/CallEvaluationTextStatus.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from "./CallEvaluationTextStatus.module.scss";
+import cx from 'classnames';
 
 export interface ICallEvaluationTextStatus {
   evaluationStatus: "excellent" | "good" | "bad";
@@ -7,8 +8,22 @@ export interface ICallEvaluationTextStatus {
 
 const CallEvaluationTextStatus: React.FC<ICallEvaluationTextStatus> = ({ evaluationStatus }) => {
   return (
-    <div className={styles.CallEvaluationTextStatus}>
-      {evaluationStatus}
+    <div className={cx(styles.callEvaluationStatusText,
+      evaluationStatus === "excellent"
+      ? styles.excellent
+      : evaluationStatus === "good"
+      ? styles.good
+      : evaluationStatus === "bad"
+      ? styles.bad
+      : null
+  )}>
+      {evaluationStatus === "excellent"
+      ? "Отлично"
+      : evaluationStatus === "good"
+      ? "Хорошо"
+      : evaluationStatus === "bad"
+      ? "Плохо"
+      : null} 
      </div>  
   )
   
