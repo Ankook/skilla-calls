4ededc5d45f30f7f46a45632f79161c9b42d15f1
Ankook <52046510+Ankook@users.noreply.github.com>
2022-06-03 22:42:11 +0300

    adding routes, consts

---
+++ my-app/src/services/routes.ts
@@ -0,0 +1,39 @@
+
+
+export const navigationRoutes = [
+  {
+    path: HOME_PAGE,
+    img: <HomeIcon />,
+    name: "Главная",
+  },
+  {
+    path: USERS_PAGE,
+    img: <UsersIcon />,
+    name: "Пользователи",
+  },
+  {
+    path: TEMPLATE_PAGE,
+    img: <TemplateIcon />,
+    name: "Шаблоны настроек",
+  },
+  {
+    path: AUTOREGISTRATION_PAGE,
+    img: <AutoRegistrationIcon />,
+    name: "Авторегистрация",
+  },
+  {
+    path: SESSIONS_PAGE,
+    img: <SessionIcon />,
+    name: "Сессии",
+  },
+  {
+    path: ACCOUNTS_PAGE,
+    img: <AccountsIcon />,
+    name: "Аккаунты",
+  },
+  {
+    path: MAIL_PAGE,
+    img: <MailIcon />,
+    name: "Почты",
+  },
+];
