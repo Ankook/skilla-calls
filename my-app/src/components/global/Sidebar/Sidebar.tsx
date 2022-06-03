d45fbe023e6e085faf7abf743f6e7d9112c0973a
Ankook <52046510+Ankook@users.noreply.github.com>
2022-06-03 22:39:06 +0300

    copying sidebar code

--- my-app/src/components/global/Sidebar/Sidebar.tsx
+++ my-app/src/components/global/Sidebar/Sidebar.tsx
@@ -14,3 +14,58 @@ const Sidebar: React.FC<SidebarPropsType> = () => {
 } 
 
 export { Sidebar }
+
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./Navbar.module.scss";
+import Logo from "../../../assets/logo.svg";
+import { ButtonRoute } from "./ButtonRoute";
+import { navigationRoutes } from "../../../services/routes";
+import { useAppSelector } from "../../../hooks/redux";
+import { RootState } from "../../../redux/store";
+import { Roles } from "../../../redux/auth/types/roles";
+import { USERS_PAGE } from "../../../services/consts";
+
+const Navbar = () => {
+  const role = useAppSelector((state: RootState) => state.authReducer.roles);
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.logoWrapper}>
+        <Link to={"/"}>
+          <img src={Logo} alt="" />
+        </Link>
+      </div>
+      <nav className={styles.nav}>
+        <ul className={styles.ul}>
+          <li className={styles.li}>
+            {role === Roles.ADMIN &&
+              navigationRoutes.map((item, index) => (
+                <ButtonRoute
+                  name={item.name}
+                  img={item.img}
+                  key={index}
+                  path={item.path}
+                />
+              ))}
+            {(role === Roles.USER ||
+              role === Roles.TESTER ||
+              role === Roles.LIKER) &&
+              navigationRoutes
+                .filter((route) => route.path !== USERS_PAGE)
+                .map((item, index) => (
+                  <ButtonRoute
+                    name={item.name}
+                    img={item.img}
+                    key={index}
+                    path={item.path}
+                  />
+                ))}
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export { Navbar };
+
