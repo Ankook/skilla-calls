ffbc8b401778ff13825a6a9df6a55dc818c32e82
Ankook <52046510+Ankook@users.noreply.github.com>
2022-06-07 21:01:47 +0300

    removing extra imports

--- my-app/src/components/pages/documentsPage/DocumentsPage.tsx
+++ my-app/src/components/pages/documentsPage/DocumentsPage.tsx
@@ -1,6 +1,5 @@
 import React from 'react'; 
-import { CallEvaluationTextStatus } from '../../Ui-Kit/CallEvaluationTextStatus';
-import { IsWeb } from '../../Ui-Kit/IsWeb';
+
 
 export interface CallsPagePropsType {
 }
